ba89ba29b8bdd0f9a555a5be0c0debd62a037479
Oren Yomtov <thenameisoren@gmail.com>
2020-09-24 13:03:22 +0300

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# iot-demo-webapp
+# IoT Demo Web App
+
+## Quickstart
+```bash
+python app.py
+```
+The webapp should now be available at [http://localhost/:9000](http://localhost:9000/)
---
+++ app.py
@@ -0,0 +1,73 @@
+import platform
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+<!doctype html>
+<html>
+<head>
+  <meta charset = "utf-8">
+  <title>IoT Demo Web App</title>
+  <style>
+    html, body, iframe {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        border-width: 0;
+    }
+  </style>
+</head>
+<body>
+  <iframe></iframe>
+
+  <script>
+      // Reload the iframe every 0.2 seconds
+      setInterval(async () => {
+          let r = await fetch('/frame?' + Date.now());
+          frames[0].document.documentElement.innerHTML = await r.text();
+      }, 200)
+  </script>
+</body>
+</html>
+"""
+
+@app.route('/frame')
+def frame():
+    return """
+<!doctype html>
+<html>
+<head>
+  <meta charset = "utf-8">
+  <title>IoT Demo Web App Frame</title>
+  <style>
+    html, body {{
+        height: 100%;
+        margin: 0;
+        font-family: Helvetica, Arial;
+        text-align: center;
+        color: white;
+        display: grid;
+        font-size: 40px;
+        background-color: green;
+    }}
+    div {{
+        justify-self: center;
+        align-self: center;
+    }}
+  </style>
+</head>
+<body>
+    <div>
+        <h1>{}</h1>
+        <h1>{}</h1>
+    </div>
+</body>
+</html>
+""".format(platform.python_version(), datetime.now().strftime("%H:%M:%S.%f"))
+
+if __name__ == '__main__':
+    app.run(port=9000)
