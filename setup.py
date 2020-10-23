f168f748cfef3e8fa5031e408366bcd10fafdc43
Oren Yomtov <thenameisoren@gmail.com>
2020-10-23 12:30:05 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='iot-demo-webapp',
-    version='1.1.0',
+    version='1.2.104',
     description='Simple webapp for demo purposes.',
     py_modules=['iot-demo-webapp'],
     install_requires=['Flask==1.0.4'],
