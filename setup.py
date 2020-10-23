9aefe26dd083eb26ce655d327cd46f7b4eb98b74
Oren Yomtov <thenameisoren@gmail.com>
2020-10-23 12:32:31 +0300

    Flask 1.1.2

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 
 setup(
     name='iot-demo-webapp',
-    version='1.2.104',
+    version='1.2.112',
     description='Simple webapp for demo purposes.',
     py_modules=['iot-demo-webapp'],
-    install_requires=['Flask==1.0.4'],
+    install_requires=['Flask==1.1.2'],
     include_package_data=True,
     package_data = {'': ['LICENSE']}
 )
